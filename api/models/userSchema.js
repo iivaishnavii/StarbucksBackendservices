86a129cfebf50f3cf30fb0d4317ac052e3ddac0f
KavyaChennoju97 <kavya.chennoju@sjsu.edu>
2019-05-09 12:53:26 -0700

    starter code

---
+++ api/models/userSchema.js
@@ -0,0 +1,24 @@
+var uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+let userSchema = new Schema({
+    pin: {
+        type: String, 
+    },
+    email: {
+        type: String, 
+        },
+
+    phone: {
+        type: String, 
+        },  
+    city: {
+        type: Array, 
+
+    },
+
+});
+
+module.exports = mongoose.model('User', userSchema);
